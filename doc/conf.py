b919f712cffba0947794db7d096d87f40d97d9c4
fsuarez6 <fsuarez6@gmail.com>
2017-10-23 15:54:16 +0800

    Specify minimal Sphinx version

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ catkin_package = catkin_pkg.package.parse_package(m)
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
